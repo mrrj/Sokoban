083807dcf3218473a8374e85e7fef9cf3f0c905b
mrrj <marja.rolandsson@gmail.com>
2021-08-10 16:02:59 +0200

    addes xpos, ypos

--- SokobanGame.js
+++ SokobanGame.js
@@ -1,11 +1,15 @@
 "use strict";
-var xPos, yPos;
+
+var playerXPos, playerYPos;
 
 var mapGrid = document.getElementById("map");
 
-function addTile(tileValue, tileId) {
+document.getElementById("map").addEventListener("keypress", arrowKeys);
+
+function addTile(tileValue, xPos, yPos) {
 
    let newTile = document.createElement("div");
+   let tileId = "x" + xPos + "y" + yPos;
    newTile.id = tileId;
    newTile.classList.add("block");
 
@@ -17,6 +21,8 @@ function addTile(tileValue, tileId) {
    }
    else if(tileValue === "P") {
        newTile.classList.add("entity-player");
+       playerXPos = xPos;
+       playerYPos = yPos;
    }
    else if(tileValue === "G"){
        newTile.classList.add("tile-goal");
@@ -35,9 +41,8 @@ function createGrid(){
 
     for(let i = 0; i < map.length; i++){
         for(let j = 0; j < map[i].length; j++){
-            let id = "x" + i + "y" + j;
             let value = map[i][j][0];
-            addTile(value, id);
+            addTile(value, i, j);
         }
     }
 
