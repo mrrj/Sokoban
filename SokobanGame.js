bc6fff66008e5bdf550e14c849a4098ebdcbc69b
mrrj <marja.rolandsson@gmail.com>
2021-08-12 09:44:44 +0200

    made player movable

--- SokobanGame.js
+++ SokobanGame.js
@@ -4,7 +4,7 @@ var playerXPos, playerYPos;
 
 var mapGrid = document.getElementById("map");
 
-document.getElementById("map").addEventListener("keypress", arrowKeys);
+window.addEventListener("keydown", arrowKeys);
 
 function addTile(tileValue, xPos, yPos) {
 
@@ -42,19 +42,67 @@ function createGrid(){
     for(let i = 0; i < map.length; i++){
         for(let j = 0; j < map[i].length; j++){
             let value = map[i][j][0];
-            addTile(value, i, j);
+            addTile(value, j, i);
         }
     }
 
 }
 
-function arrowKeys(){
-    /**check if key pressed was arrow
-     * if true run movePlayer(xPosChange, yPosChange)
-     */
+function arrowKeys(e){
+    console.log("running arrowKeys");
+
+    let arrow = e.keyCode;
+
+    switch(arrow){
+        //left arrow
+        case 37:
+            e.preventDefault();
+            movePlayer(-1,0);
+            break;
+        //up arrow    
+        case 38:
+            e.preventDefault();
+            movePlayer(0,-1);
+            break;
+        //right arrow
+        case 39:
+            e.preventDefault(); 
+            movePlayer(1,0);
+            break;
+        //down arrow
+        case 40:
+            e.preventDefault();
+            movePlayer(0,1);
+            break;
+        default:
+            break;
+
+    }
 }
 
 function movePlayer(xPosChange, yPosChange){
+
+    let currentElementId = "x" + playerXPos + "y" + playerYPos;
+    let currentElement = document.getElementById(currentElementId);
+    currentElement.classList.replace("entity-player", "tile-space");
+
+    playerXPos += xPosChange;
+    playerYPos += yPosChange;
+
+    let elementToId = "x" + playerXPos + "y" + playerYPos;
+    let elementTo = document.getElementById(elementToId);   
+    elementTo.classList.replace("tile-space", "entity-player");
+
+    /**TODO:
+     * update css for currentElement and elementTo
+     * also what if player moves a block?
+     * 
+     * playerCanMove(class of elementTo)
+     */
+
+}
+
+function playerCanMove(elementCLass){
 
 }
 
