2f9a001faef8323badd9b9e340f6a7f4e7265716
mrrj <marja.rolandsson@gmail.com>
2021-08-16 16:39:37 +0200

    fully functional but kinda ugly

--- SokobanGame.js
+++ SokobanGame.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var playerXPos, playerYPos;
+var currentElementClass = "tile-space";
 
 var mapGrid = document.getElementById("map");
 
@@ -8,39 +9,38 @@ window.addEventListener("keydown", arrowKeys);
 
 function addTile(tileValue, xPos, yPos) {
 
-   let newTile = document.createElement("div");
-   let tileId = "x" + xPos + "y" + yPos;
-   newTile.id = tileId;
-   newTile.classList.add("block");
+    let newTile = document.createElement("div");
+    let tileId = "x" + xPos + "y" + yPos;
+    newTile.id = tileId;
 
-   if(tileValue === "W"){
-       newTile.classList.add("tile-wall");
-   }
-   else if(tileValue === "B"){
-       newTile.classList.add("entity-block");
-   }
-   else if(tileValue === "P") {
-       newTile.classList.add("entity-player");
-       playerXPos = xPos;
-       playerYPos = yPos;
-   }
-   else if(tileValue === "G"){
-       newTile.classList.add("tile-goal");
-   }
-   else{
-       newTile.classList.add("tile-space");
-   }
+    if (tileValue === "W") {
+        newTile.classList.add(Tiles.Wall);
+    }
+    else if (tileValue === "B") {
+        newTile.classList.add(Entities.Block);
+    }
+    else if (tileValue === "P") {
+        newTile.classList.add(Entities.Character);
+        playerXPos = xPos;
+        playerYPos = yPos;
+    }
+    else if (tileValue === "G") {
+        newTile.classList.add(Tiles.Goal);
+    }
+    else {
+        newTile.classList.add(Tiles.Space);
+    }
 
-   mapGrid.appendChild(newTile);
+    mapGrid.appendChild(newTile);
 
 }
 
-function createGrid(){
+function createGrid() {
 
     let map = tileMap01.mapGrid;
 
-    for(let i = 0; i < map.length; i++){
-        for(let j = 0; j < map[i].length; j++){
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[i].length; j++) {
             let value = map[i][j][0];
             addTile(value, j, i);
         }
@@ -48,31 +48,31 @@ function createGrid(){
 
 }
 
-function arrowKeys(e){
+function arrowKeys(e) {
     console.log("running arrowKeys");
 
     let arrow = e.keyCode;
 
-    switch(arrow){
+    switch (arrow) {
         //left arrow
         case 37:
             e.preventDefault();
-            movePlayer(-1,0);
+            movePlayer(-1, 0);
             break;
         //up arrow    
         case 38:
             e.preventDefault();
-            movePlayer(0,-1);
+            movePlayer(0, -1);
             break;
         //right arrow
         case 39:
-            e.preventDefault(); 
-            movePlayer(1,0);
+            e.preventDefault();
+            movePlayer(1, 0);
             break;
         //down arrow
         case 40:
             e.preventDefault();
-            movePlayer(0,1);
+            movePlayer(0, 1);
             break;
         default:
             break;
@@ -80,31 +80,97 @@ function arrowKeys(e){
     }
 }
 
-function movePlayer(xPosChange, yPosChange){
+
+//return true if player moves, false otherwise
+function movePlayer(xPosChange, yPosChange) {
+
 
     let currentElementId = "x" + playerXPos + "y" + playerYPos;
     let currentElement = document.getElementById(currentElementId);
-    currentElement.classList.replace("entity-player", "tile-space");
+
+    let nextElement = getNextElement(xPosChange, yPosChange);
+
+    let nextElementClass = nextElement.className;
+
+    if (nextElement.classList.contains("tile-space")) {
+        moveTo(nextElement, nextElementClass, "entity-player");
+        moveFrom(currentElement, "entity-player", currentElementClass);
+    }
+    else if (nextElement.classList.contains("tile-goal")) {
+        moveTo(nextElement, nextElementClass, "entity-player");
+        moveFrom(currentElement, "entity-player", currentElementClass);
+    }
+    else if (nextElement.classList.contains("entity-block")) {
+        nextElementClass = "tile-space";
+
+        let nextBlockPosition = getNextElement(2*xPosChange,2*yPosChange);
+
+        if(nextBlockPosition.classList.contains("tile-space")){
+            nextBlockPosition.classList.replace("tile-space","entity-block");
+            nextElement.classList.replace("entity-block","entity-player");
+            currentElement.classList.replace("entity-player", currentElementClass);
+        }
+        else if(nextBlockPosition.classList.contains("tile-goal")){
+            nextBlockPosition.classList.replace("tile-goal","entity-block-goal");
+            nextElement.classList.replace("entity-block","entity-player");
+            currentElement.classList.replace("entity-player", currentElementClass);
+        }
+        else {
+            return false;
+        }        
+    }
+    else if(nextElement.classList.contains("entity-block-goal")){
+        nextElementClass = "tile-goal";
+
+        let nextBlockPosition = getNextElement(2*xPosChange,2*yPosChange);
+
+        if(nextBlockPosition.classList.contains("tile-space")){
+            nextBlockPosition.classList.replace("tile-space","entity-block");
+            nextElement.classList.replace("entity-block-goal","entity-player");
+            currentElement.classList.replace("entity-player", currentElementClass);
+        }
+        else if(nextBlockPosition.classList.contains("tile-goal")){
+            nextBlockPosition.classList.replace("tile-goal","entity-block-goal");
+            nextElement.classList.replace("entity-block-goal","entity-player");
+            currentElement.classList.replace("entity-player", currentElementClass);
+        }
+        else {
+            return false;
+        }    
+
+    }
+    else return false;
 
     playerXPos += xPosChange;
     playerYPos += yPosChange;
+    currentElementClass = nextElementClass;
 
-    let elementToId = "x" + playerXPos + "y" + playerYPos;
-    let elementTo = document.getElementById(elementToId);   
-    elementTo.classList.replace("tile-space", "entity-player");
-
-    /**TODO:
-     * update css for currentElement and elementTo
-     * also what if player moves a block?
-     * 
-     * playerCanMove(class of elementTo)
-     */
-
-}
-
-function playerCanMove(elementCLass){
+    return true;
 
 }
 
 
+function moveTo(toElement, tileClass, entityClass) {
+    toElement.classList.replace(tileClass, entityClass);
+}
+
+function moveFrom(fromElement, entityClass, tileClass) {
+    fromElement.classList.replace(entityClass, tileClass);
+}
+
+
+function getNextElement(xPosChange, yPosChange) {
+
+    let currentXPos = playerXPos;
+    let currentYPos = playerYPos;
+
+    let nextPlayerXPos = currentXPos += xPosChange;
+    let nextPlayerYPos = currentYPos += yPosChange;
+
+    let nextElementId = "x" + nextPlayerXPos + "y" + nextPlayerYPos;
+    let nextElement = document.getElementById(nextElementId);
+
+    return nextElement;
+
+}
 
